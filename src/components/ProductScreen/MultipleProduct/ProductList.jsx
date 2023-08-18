17b3e79aa83cfb404d31eb0c3fc1c050c4f40921
Milan <deymilan312@gmail.com>
2023-08-18 13:44:29 +0530

    Implemented add-to-cart from search screen

--- src/components/ProductScreen/MultipleProduct/ProductList.jsx
+++ src/components/ProductScreen/MultipleProduct/ProductList.jsx
@@ -1,13 +1,19 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
 import { useNavigate } from 'react-router-dom';
 import StarRating from '../../StarRating';
+import { Store } from '../../../Store';
 
 function ProductList(product) {
     const navigate = useNavigate();
+    const [productQuantity, setProductQuantity] = useState(1)
+    const { state, dispatch: ctxDispatch } = useContext(Store);
+    const addToCartHandler = async () => {
+        ctxDispatch({ type: 'CART_ADD_ITEM', payload: { ...product.product, quantity: productQuantity } })
+    }
     return (
-        <div className="card mb-3" onClick={() => navigate(`/product/${product.product._id}`)}>
+        <div className="card mb-3">
             <div className="row g-0">
-                <div className="col-md-4">
+                <div className="col-md-4" onClick={() => navigate(`/product/${product.product._id}`)}>
                     <img type='button' src={product.product.thumbnail} className="img-fluid object-fit-fill rounded-start h-100 w-100" alt={product.product.title} />
                 </div>
                 <div className="col-md-8 my-auto">
@@ -22,6 +28,7 @@ function ProductList(product) {
                         <h4 className='d-flex'>
                             <StarRating value={product.product.rating} editable={false} />
                         </h4>
+                        <button onClick={addToCartHandler} className="btn btn-danger rounded-pill float-end m-2" type="button">Add to Cart</button>
                     </div>
                 </div>
             </div>
